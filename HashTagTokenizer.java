683e91b04403f45d24db9bb6f64bafafb310d115
orital8 <ori.tal01@post.runi.ac.il>
2024-01-30 18:47:20 +0200

    first try

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -8,19 +8,28 @@ public class HashTagTokenizer {
 		String []dictionary = readDictionary("dictionary.txt");
 		breakHashTag(hashTag, dictionary);
 	}
-
+	//take file and return array of word called dictionary
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
 
 		In in = new In(fileName);
 
-		// Your code here
+		for(int i = 0 ; i < 3000; i++){
+			dictionary[i] = in.readString();
+		}
 
 		return dictionary;
 	}
 
+	//take word and array of dictionary and return true if word exist in dictionary
 	public static boolean existInDictionary(String word, String []dictionary) {
-		// Your code here
+		boolean exist = false;
+		for(int i = 0 ; i < 3000; i++){
+			if (dictionary[i].equals(word)){
+				exist = true;
+			} 
+		}
+		return exist;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
@@ -29,12 +38,18 @@ public class HashTagTokenizer {
         if (hashtag.isEmpty()) {
             return;
         }
- 
-        int N = hashtag.length();
-
+        int N = hashtag.length()-1;
+		String check = hashtag;
+		check = check.toLowerCase();
         for (int i = 1; i <= N; i++) {
-		
-        }
+				if(existInDictionary(check.substring(0, i), dictionary) == true){
+					System.out.println(check.substring(0, i));
+					check = check.substring(i);
+					  breakHashTag(check, dictionary);
+				}
+				
+		}
+			
     }
 
 }
